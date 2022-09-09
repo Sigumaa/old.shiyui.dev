6e7805c991dec513e92ae6efbf0b5fbf11c4ff2b
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-09-09 15:13:21 +0900

    feat: add icon

--- src/components/me.rs
+++ src/components/me.rs
@@ -5,7 +5,7 @@ pub fn me() -> Html {
     html! {
         <div>
             <h1>{"Shiyui"}</h1>
-            <img class="logo" src="https://raw.githubusercontent.com/Sigumaa/Sigumaa/c24583e8d0ed0e62056f1c2c1567744f9b298d51/shiyui.jpeg" alt="my icon" />
+            <img class="logo" src="https://github.com/Sigumaa/Sigumaa/blob/main/shiyui.jpeg?raw=true" alt="my icon" />
         </div>
     }
 }
