ea3fdc0a1b71cd20379b33090d549d53c4d27505
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-11-27 15:09:47 +0900

    feat: pls ogp

--- dist/shiyuidev-1797a7acf2c780bc.js
+++ dist/shiyuidev-1797a7acf2c780bc.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('shiyuidev-577a06ef4079ae1c_bg.wasm', import.meta.url);
+        input = new URL('shiyuidev-1797a7acf2c780bc_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
